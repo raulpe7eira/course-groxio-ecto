3588fad7e357cac8f3f8ef95912305bed24a3eb8
Raul Pereira <raul.pereira@gmail.com>
2022-12-27 19:04:29 -0300

    6. ecto associations

---
+++ tome/lib/tome/feedback/rating.ex
@@ -0,0 +1,21 @@
+defmodule Tome.Feedback.Rating do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Tome.Library.Book
+
+  schema "ratings" do
+    field(:stars, :integer)
+    belongs_to(:book, Book)
+
+    timestamps()
+  end
+
+  def new(book), do: Ecto.build_assoc(book, :ratings)
+
+  def changeset(rating, params \\ %{}) do
+    rating
+    |> cast(params, [:stars, :book_id])
+    |> validate_required([:stars, :book_id])
+  end
+end
--- tome/lib/tome/library/book.ex
+++ tome/lib/tome/library/book.ex
@@ -2,12 +2,16 @@ defmodule Tome.Library.Book do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Tome.Feedback.Rating
+
   schema "books" do
     field(:title, :string)
     field(:isbn, :string)
     field(:description)
     field(:published_on, :date, default: nil)
     field(:status, :string, default: "working")
+
+    has_many(:ratings, Rating)
 
     timestamps()
   end
